fefbf6c061589117e031eb8b4f3b2f2e86b47b29
Пользователь <suhrob.coder0604@gmail.com>
2023-09-07 15:37:13 +0300

    finish

--- src/js/app.js
+++ src/js/app.js
@@ -125,3 +125,154 @@ videoWrappper?.addEventListener("click", (e) => {
     }
   }, 100);
 });
+
+const loc = location.pathname;
+const homeLink = document.querySelectorAll(".home-link");
+const servicesLink = document.querySelectorAll(".services-link");
+const cooperationsLink = document.querySelectorAll(".cooperations-link");
+const aboutLink = document.querySelectorAll(".about-link");
+const contactsLink = document.querySelectorAll(".contacts-link");
+console.log(contactsLink[1]);
+
+const linksData = {
+  forHome: [
+    "/uz",
+    "/en",
+    "/ru",
+
+    "/index",
+    "/uz/index",
+    "/ru/index",
+    "/en/index",
+
+    "/index.html",
+    "/uz/index.html",
+    "/ru/index.html",
+    "/en/index.html",
+  ],
+
+  forServices: [
+    "/treatment",
+    "uz/treatment",
+    "ru/treatment",
+    "en/treatment",
+
+    "/treatment.html",
+    "/uz/treatment.html",
+    "/ru/treatment.html",
+    "/en/treatment.html",
+
+    "/diagnostics",
+    "/uz/diagnostics",
+    "/ru/diagnostics",
+    "/en/diagnostics",
+
+    "/diagnostics.html",
+    "/uz/diagnostics.html",
+    "/ru/diagnostics.html",
+    "/en/diagnostics.html",
+
+    "/information",
+    "/uz/information",
+    "/ru/information",
+    "/en/information",
+
+    "/information.html",
+    "/uz/information.html",
+    "/ru/information.html",
+    "/en/information.html",
+
+    "/trainings",
+    "/uz/trainings",
+    "ru/trainings",
+    "en/trainings",
+
+    "/trainings.html",
+    "/uz/trainings.html",
+    "ru/trainings.html",
+    "en/trainings.html",
+  ],
+
+  forCooperations: [
+    "/partners",
+    "/uz/partners",
+    "/ru/partners",
+    "/en/partners",
+
+    "/partners.html",
+    "/uz/partners.html",
+    "/ru/partners.html",
+    "/en/partners.html",
+
+    "/offer",
+    "/uz/offer",
+    "/ru/offer",
+    "/en/offer",
+
+    "/offer.html",
+    "/uz/offer.html",
+    "/ru/offer.html",
+    "/en/offer.html",
+  ],
+
+  forAbout: [
+    "/team",
+    "/uz/team",
+    "/ru/team",
+    "/en/team",
+
+    "/team.html",
+    "/uz/team.html",
+    "/ru/team.html",
+    "/en/team.html",
+
+    "/documents",
+    "/uz/documents",
+    "/ru/documents",
+    "/en/documents",
+
+    "/documents.html",
+    "/uz/documents.html",
+    "/ru/documents.html",
+    "/en/documents.html",
+
+    "/faq",
+    "/uz/faq",
+    "/ru/faq",
+    "/en/faq",
+
+    "/faq.html",
+    "/uz/faq.html",
+    "/ru/faq.html",
+    "/en/faq.html",
+  ],
+
+  forContacts: [
+    "/contacts",
+    "/uz/contacts",
+    "/ru/contacts",
+    "/en/contacts",
+
+    "/contacts.html",
+    "/uz/contacts.html",
+    "/ru/contacts.html",
+    "/en/contacts.html",
+  ],
+};
+
+function checkAvtiveLink(arr, link) {
+  for (let i = 0; i < arr.length; i++) {
+    if (loc == arr[i]) {
+      link?.classList?.add("active-link");
+      break;
+    }
+  }
+}
+
+for (let i = 0; i < 2; i++) {
+  checkAvtiveLink(linksData.forHome, homeLink?.[i]);
+  checkAvtiveLink(linksData.forServices, servicesLink?.[i]);
+  checkAvtiveLink(linksData.forCooperations, cooperationsLink?.[i]);
+  checkAvtiveLink(linksData.forAbout, aboutLink?.[i]);
+  checkAvtiveLink(linksData.forContacts, contactsLink?.[i]);
+}
